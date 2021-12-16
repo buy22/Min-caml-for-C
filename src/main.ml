b49fa5d13c90febbbd0d5832a29cb1ddcc384934
buy22 <buyu2017@126.com>
2021-12-16 02:08:43 -0600

    success test

--- src/main.ml
+++ src/main.ml
@@ -1,9 +1,11 @@
 open Core
 open Ast
 
-
 let ss =
   let lexbuf = Lexing.from_channel In_channel.stdin in
-  Parser.program Lexer.token lexbuf 
+  let result = Parser.program Lexer.token lexbuf in
+  (* output ast *)
+  result |> string_of_prog |> print_string;
+     Out_channel.newline stdout
 
 let _ = ss
